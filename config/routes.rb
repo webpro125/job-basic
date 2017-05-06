3e6e8db8dfc3f89389f669b04d55131937c3bdee
devpro125 <web.devpro125@gmail.com>
2017-05-06 23:53:04 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users, path: '/', path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      sign_up: 'registration'
+  }, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+
+  root 'home#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
